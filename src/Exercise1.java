1ead6f32c4029de398aa64a54bde3257f1221190
AdrianRaulVlad <42966377+AdrianRaulVlad@users.noreply.github.com>
2024-03-30 21:46:22 +0200

    Update Exercise1.java with a starting message

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Exercise1 {
     public static void calculateTheSumOfInteger() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Starting exercise 1");
         System.out.println("Insert numbers separated by a space to calculate the sum:");
 
         String inputNumber = scanner.nextLine();
