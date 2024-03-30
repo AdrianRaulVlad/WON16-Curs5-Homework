535b1437cad86f6350603ecbca716aef64ea7155
AdrianVlad <adrianraul.vlad@gmail.com>
2024-03-31 00:34:37 +0200

    Add the 4th exercise from homework

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         System.out.println("This is the homework for course number 5");
+        Exercise4.fundRaisingDonations();
     }
 }
